6a162d504a7e10053f4da3b4e952606fac8079d6
matteomaske <matteomascherin@gmail.com>
2025-07-10 21:48:51 +0200

    Logging in database added

--- data/database.py
+++ data/database.py
@@ -2,6 +2,10 @@ import pandas as pd
 
 from typing import List, Dict
 from data.houses import House
+from utils.logger import get_logger
+
+logger = get_logger(__name__)
+
 
 class Database:
     def __init__(self, database_path):
@@ -14,9 +18,9 @@ class Database:
         if database_path:
             dataframe = pd.read_csv(database_path)
             self.database = House.from_dataframe(dataframe)
-            print(f"Database initialized with {len(self.database)} houses.")
+            logger.info(f"Database initialized with {len(self.database)} houses.")
 
-    def get_houses(self, slots: Dict[str, str], first_n = 5) -> List[House]:
+    def get_houses(self, slots: Dict[str, str], first_n=5) -> List[House]:
         """Get all houses from the database that match the given slots."""
 
         # Filter slots values to match the database types
@@ -24,16 +28,20 @@ class Database:
             house_bhk = slots.get("house_bhk")
             if isinstance(house_bhk, str):
                 if " " in house_bhk:
-                    house_bhk = [int(word) for word in house_bhk.split() if word.isdigit()]
+                    house_bhk = [
+                        int(word) for word in house_bhk.split() if word.isdigit()
+                    ]
                 else:
                     house_bhk = [int(house_bhk)]
             house_bhk = min(house_bhk) if house_bhk else 0
             house_bhk = [i for i in range(house_bhk, 6)]
-            
+
             house_size = slots.get("house_size")
             if isinstance(house_size, str):
                 if " " in house_size:
-                    house_size = [int(word) for word in house_size.split() if word.isdigit()]
+                    house_size = [
+                        int(word) for word in house_size.split() if word.isdigit()
+                    ]
                 else:
                     house_size = [int(house_size)]
             house_size = min(house_size) if house_size else 0
@@ -41,7 +49,9 @@ class Database:
             house_rent = slots.get("house_rent")
             if isinstance(house_rent, str):
                 if " " in house_rent:
-                    house_rent = [int(word) for word in house_rent.split() if word.isdigit()]
+                    house_rent = [
+                        int(word) for word in house_rent.split() if word.isdigit()
+                    ]
                 else:
                     house_rent = [int(house_rent)]
             house_rent = max(house_rent) if house_rent else 1000000
@@ -53,20 +63,23 @@ class Database:
             house_furnished = slots.get("house_furnished")
             house_furnished = house_furnished.lower()
 
-            print(f"Filtering houses with BHK: {house_bhk}, Size: {house_size}, Rent: {house_rent}, Location: {house_location}, City: {house_city}, Furnished: {house_furnished}")
+            logger.info(
+                f"Filtering houses with BHK: {house_bhk}, Size: {house_size}, Rent: {house_rent}, Location: {house_location}, City: {house_city}, Furnished: {house_furnished}"
+            )
 
-            filter_func = lambda house: house.bhk in house_bhk and \
-                house.size >= house_size and \
-                house.rent <= house_rent and \
-                house_city in house.city and \
-                house_furnished in house.furnishing_status and \
-                house_location in house.area_locality
+            filter_func = (
+                lambda house: house.bhk in house_bhk
+                and house.size >= house_size
+                and house.rent <= house_rent
+                and house_city in house.city
+                and house_furnished in house.furnishing_status
+                and house_location in house.area_locality
+            )
 
             # Filter houses based on the slots
             filtered_houses = list(filter(filter_func, self.database))
         except Exception as e:
-            print("Error in filtering the houses")
+            logger.error("Error in filtering the houses: %s", e)
             return []
 
-
-        return filtered_houses[:min(len(filtered_houses), first_n)]
+        return filtered_houses[: min(len(filtered_houses), first_n)]
